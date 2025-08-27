4d77eaad209ab1d3a502a71a00ae5c967752bc8d
Ryan Chan <tup08628@temple.edu>
2025-08-27 14:10:58 -0400

    added comments for readablility

--- random.c
+++ random.c
@@ -4,16 +4,16 @@
 
 
 void rand_string(char *s, size_t size){
-    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int i; 
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";   //create a character set to choose from
+    int i;  //loop variable
 
-    srand ((unsigned)time(NULL));
+    srand ((unsigned)time(NULL));   //seed the random number generator
 
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {    //generate random string
         int key = rand() % (int)(sizeof(charset) - 1);
         s[i] = charset[key];
     }
 
-    s[size] = '\0';
+    s[size] = '\0';  //null terminate the string
 }
 

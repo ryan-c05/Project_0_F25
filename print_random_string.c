dfd05b0542121670fbd54c9b073d28228b6833a5
Ryan Chan <tup08628@temple.edu>
2025-08-27 14:09:51 -0400

    Created random.c file to inplement rand_string func

--- print_random_string.c
+++ print_random_string.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+char* rand_string(char *s, size_t size);
 
 // random string generator wrapper function
 char* rand_string_alloc(size_t size)
--- random.c
+++ random.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+void rand_string(char *s, size_t size){
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int i; 
+
+    srand ((unsigned)time(NULL));
+
+    for (i = 0; i < size; i++) {
+        int key = rand() % (int)(sizeof(charset) - 1);
+        s[i] = charset[key];
+    }
+
+    s[size] = '\0';
 }
+
